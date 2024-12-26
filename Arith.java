8ce6a6f9de093c73baf68fa65768fc9ebee783fb
Badduluri Nithin kumar <nithinkumarnk7893@gmail.com>
2024-12-26 14:38:25 +0530

    Add files via upload

---
+++ Arith.java
@@ -0,0 +1,18 @@
+class Arith
+{
+public static void main(String args[])
+{
+int a=4,b=4;
+System.out.println("***PROGRAM ON COMPOUND ARITHMETIC OPERATORS***");
+a+=b;
+System.out.println("the value of a="+a);
+a-=b;
+System.out.println("the value of a="+a);
+a/=b;
+System.out.println("the value of a="+a);
+a*=b;
+System.out.println("the value of a="+a);
+a%=b;
+System.out.println("the value of a="+a);
+}
+}
