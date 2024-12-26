8a7823534bbec4ecf694069fb7a0f88f576d86da
Badduluri Nithin kumar <nithinkumarnk7893@gmail.com>
2024-12-26 14:39:42 +0530

    Add files via upload

---
+++ Bool.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Bool
+{
+public static void main(String args[])
+{
+boolean a;
+Scanner s=new Scanner(System.in);
+System.out.println("enter a boolean value:");
+a=s.nextBoolean();
+System.out.println("boolean value a="+a);
+}
+}
